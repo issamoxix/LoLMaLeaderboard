48584c7abaedd21c02d4dbf9ea8229bdb5d4805d
Issamoxix <issamhaidaoui@gmail.com>
2021-02-06 16:48:45 +0100

    util test2

---
+++ util/dbc.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+import nextConnect from "next-connect";
+const url =
+  "mongodb+srv://admin1:issamroot123@lolma.aan0s.mongodb.net/users?retryWrites=true&w=majority";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function database(req, res, next) {
+  if (!client.isConnected()) await client.connect();
+  req.dbClient = client;
+  req.db = client
+    .db("lolrank")
+    .collection("users")
+    .find()
+    .sort({ rank_all: -1 })
+    .toArray();
+  return next();
+}
+const middleware = nextConnect();
+middleware.use(database);
+export default middleware;
