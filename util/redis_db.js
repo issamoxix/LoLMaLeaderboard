b788735a9bdb3426105e2b22ae5ae2c50c99758b
Issamoxix <issamhaidaoui@gmail.com>
2021-02-23 16:15:12 +0100

    co

--- util/redis_db.js
+++ util/redis_db.js
@@ -1,5 +1,9 @@
 const redis = require("redis");
 export default function insert_data(name, rank_all, level) {
+  function decode_utf8(s) {
+    return decodeURIComponent(escape(s));
+  }
+
   console.log("Add to redis");
   const client = redis.createClient();
   client.on("error", function (error) {
@@ -7,7 +11,11 @@ export default function insert_data(name, rank_all, level) {
   });
   let rka = parseInt(rank_all);
   let lvl = parseInt(level);
-  const args = ["lolrank", rka === 0 ? lvl : rka, name && name.toLowerCase()];
+  const args = [
+    "lolrank",
+    rka === 0 ? lvl : rka,
+    name && decode_utf8(name).toLowerCase(),
+  ];
   client.zadd(args, function (addError, addResponse) {
     if (addError) throw addError;
     console.log(addResponse);
