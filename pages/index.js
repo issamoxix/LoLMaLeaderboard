2eb627672df48914f94e53ea859cc715298f3865
Issamoxix <issamhaidaoui@gmail.com>
2021-04-15 17:39:22 +0000

    nprogress thingy

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,14 @@ import HomeComp from "../components/dt/pages/Home";
 import LeaderBoard from "../components/dt/pages/LeaderBoard";
 import MobileNav from "../components/dt/MobileNav";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
+import nProgress from "nprogress";
 
+Router.onRouteChangeStart = (url) => {
+  nProgress.start();
+};
+
+Router.onRouteChangeComplete = () => nProgress.done();
 function reducer(state, action) {
   switch (action.type) {
     case "H":
@@ -112,6 +118,13 @@ function dt() {
         ></meta>
         <meta name="theme-color" content="#141726"></meta>
         <link rel="icon" href="https://lolrankmaroc.live/logo/logo3.png" />
+        {/* nprogress files */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossorigin="anonymous"
+        />
       </Head>
       {!toggle && (
         <div
