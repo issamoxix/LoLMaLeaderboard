082e198977f31485333fa7b86735709a1c98b889
Issamoxix <issamhaidaoui@gmail.com>
2021-04-24 17:57:15 +0000

    changes to seo

--- components/dt/pages/Home.js
+++ components/dt/pages/Home.js
@@ -9,6 +9,7 @@ import Table from "../Table";
 import styles from "../../../styles/dt/Home.module.css";
 import { useEffect } from "react";
 import { Data } from "../data";
+import Head from "next/head";
 
 function HomeComp() {
   const [data, setData] = useState();
@@ -42,6 +43,55 @@ function HomeComp() {
       {/* <Mcom>
 <Landing />
 </Mcom> */}
+      <Head>
+        <title>LoLRankMaroc</title>
+        <meta
+          name="description"
+          content="League of legends Moroccan Solo Q Leaderboard , ranking top Moroccan Players"
+        />
+        <meta
+          name="keywords"
+          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
+        />
+        <meta name="author" content="Issam.H" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous"
+        />
+
+        <meta
+          name="image"
+          content="https://lolrankmaroc.live/images/banner.jpg"
+        />
+        <meta
+          property="og:image"
+          content="https://lolrankmaroc.live/images/banner.jpg"
+        />
+        <meta
+          name="twitter:image"
+          content="https://lolrankmaroc.live/images/banner.jpg"
+        ></meta>
+
+        <meta property="og:title" content="lolrankmaroc.live" />
+        <meta name="twitter:title" content="lolrankmaroc.live" />
+
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://lolrankmaroc.live/" />
+        <meta
+          property="og:description"
+          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@issamoxix" />
+        <meta
+          name="twitter:description"
+          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
+        />
+      </Head>
       <animated.div style={animation}>
         <Modal show={setShow} />
       </animated.div>
--- pages/index.js
+++ pages/index.js
@@ -84,52 +84,6 @@ function dt() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>LoLRankMaroc</title>
-        <meta
-          name="description"
-          content="League of legends Moroccan Solo Q Leaderboard , ranking top Moroccan Players"
-        />
-        <meta
-          name="keywords"
-          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
-        />
-        <meta name="author" content="Issam.H" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-          crossorigin="anonymous"
-        />
-
-        <meta
-          name="image"
-          content="https://lolrankmaroc.live/images/banner.jpg"
-        />
-
-        <meta property="og:title" content="lolrankmaroc.live" />
-        <meta property="og:type" content="article" />
-        <meta property="og:url" content="https://lolrankmaroc.live/" />
-        <meta
-          property="og:image"
-          content="https://lolrankmaroc.live/images/banner.jpg"
-        />
-        <meta
-          property="og:description"
-          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
-        />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@issamoxix" />
-        <meta name="twitter:title" content="lolrankmaroc.live" />
-        <meta
-          name="twitter:description"
-          content="league of legends,lolma,lol Maroc, league of legends Maroc, Moroccan Leaderboard , Top Morrcan Player"
-        />
-        <meta
-          name="twitter:image"
-          content="https://lolrankmaroc.live/images/banner.jpg"
-        ></meta>
         <meta name="theme-color" content="#141726"></meta>
         <link rel="icon" href="https://lolrankmaroc.live/logo/logo3.png" />
         {/* nprogress files */}
