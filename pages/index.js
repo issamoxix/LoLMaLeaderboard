9ea267ab20c372fa241fe9044e67614bda4111d0
Issamoxix <issamhaidaoui@gmail.com>
2021-04-24 22:40:41 +0000

    pix

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ import nProgress from "nprogress";
 import Champions from "../components/dt/pages/Champions";
 import Loading from "../components/dt/pages/Loading";
 import ChampLadder from "../components/dt/pages/ChampLadder";
+import ReactPixel from "react-facebook-pixel";
 
 Router.onRouteChangeStart = (url) => {
   nProgress.start();
@@ -46,7 +47,31 @@ function reducer(state, action) {
   }
 }
 
+{
+  /* <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1439439406396220');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1439439406396220&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code --> */
+}
 function dt() {
+  const options = {
+    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+    debug: false, // enable logs
+  };
+  ReactPixel.init("1439439406396220", options);
+  ReactPixel.pageView();
   const [{ comp, label }, dispatch] = useReducer(reducer, {
     comp: <Loading />,
     label: "Loading ...",
