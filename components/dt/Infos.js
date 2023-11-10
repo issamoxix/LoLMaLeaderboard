4a2a49814dd0776ec8368a140d58c4671fdbae5e
Issam Haidaoui <issamhaidaoui@gmail.com>
2023-11-10 20:03:30 +0100

    update description

--- components/dt/Infos.js
+++ components/dt/Infos.js
@@ -6,9 +6,9 @@ function Infos() {
   console.log(process.env.NEXT_PUBLIC_NAME);
   return (
     <div className={styles.InfosContainer}>
-      <h2 className={styles.Infotitle}>lolrankmaroc.live v0.0.1 </h2>
+      <h2 className={styles.Infotitle}>lolrankmaroc.live v0.0.2 </h2>
       <p className={styles.para}>
-        I'm still making this website better. If you find any problems or have suggestions, let me know on my socials or add me on Discord at Issam#6576. If you want to help, click the GitHub icon below!
+        I'm still making this website better. If you find any problems or have suggestions, let me know on my socials or add me on Discord at @shodowinthemini. If you want to help, click the GitHub icon below!
       </p>
       <div className={styles.Socials}>
         <a href="https://github.com/issamoxix/LoLMaLeaderboard">
