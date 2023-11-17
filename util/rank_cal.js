a10a2c99f3a10e05379f0543de14bb20bb0672d0
Issam Haidaoui <issamhaidaoui@gmail.com>
2023-11-17 19:33:34 +0100

    Update rank_cal.js

--- util/rank_cal.js
+++ util/rank_cal.js
@@ -1,65 +1,29 @@
 export default function rank_calc(tier, rank, lp) {
-  let rank_all = 0;
-  switch (tier) {
-    case "CHALLENGER":
-      rank_all += 100000;
-      break;
-    case "GRANDMASTER":
-      rank_all += 90000;
-      break;
-    case "MASTER":
-      rank_all += 80000;
-      break;
-    case "DIAMOND":
-      rank_all += 70000;
-      break;
-    case "EMERALD":
-      rank_all += 60000;
-      break;
-    case "PLATINUM":
-      rank_all += 50000;
-      break;
-    case "GOLD":
-      rank_all += 40000;
-      break;
-    case "SILVER":
-      rank_all += 30000;
-      break;
-    case "BRONZE":
-      rank_all += 20000;
-      break;
-    case "IRON":
-      rank_all += 10000;
-      break;
-    case "Unranked":
-      rank_all += 0;
-      break;
-    default:
-      console.log("Error in calc");
-      break;
-  }
-  switch (rank) {
-    case "I":
-      rank_all += 4000;
-      break;
-    case "II":
-      rank_all += 3000;
-      break;
-    case "III":
-      rank_all += 2000;
-      break;
-    case "IV":
-      rank_all += 1000;
-      break;
-    case "":
-      rank_all += 0;
-      break;
-    default:
-      console.log("Error in calc");
-      break;
-  }
+  const tierValues = {
+    "CHALLENGER": 100000,
+    "GRANDMASTER": 90000,
+    "MASTER": 80000,
+    "DIAMOND": 70000,
+    "EMERALD": 60000,
+    "PLATINUM": 50000,
+    "GOLD": 40000,
+    "SILVER": 30000,
+    "BRONZE": 20000,
+    "IRON": 10000,
+    "Unranked": 0,
+  };
 
-  rank_all += lp != parseInt(lp) ? 0 : lp;
+  const rankValues = {
+    "I": 4000,
+    "II": 3000,
+    "III": 2000,
+    "IV": 1000,
+    "": 0,
+  };
 
-  return rank_all;
+  const tierValue = tierValues[tier] || 0;
+  const rankValue = rankValues[rank] || 0;
+  const lpValue = isNaN(parseInt(lp)) ? 0 : parseInt(lp);
+
+  return tierValue + rankValue + lpValue;
 }
